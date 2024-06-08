2bcbb0af21e782fd704d0e2704c049c69a3eb3d7
Samraat-LK789 <samraat1official8@gmail.com>
2024-06-08 19:17:14 +0530

    Offer cards and auto scroll

--- lib/pages/page2.dart
+++ lib/pages/page2.dart
@@ -1,15 +1,92 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-class Page2 extends StatelessWidget {
+class Page2 extends StatefulWidget {
+  @override
+  _Page2State createState() => _Page2State();
+}
+
+class _Page2State extends State<Page2> {
+  final ScrollController _scrollController = ScrollController();
+  final List<String> offers = ['Offer 1', 'Offer 2', 'Offer 3'];
+
+  @override
+  void initState() {
+    super.initState();
+    _autoScroll();
+  }
+
+  void _autoScroll() {
+    Timer.periodic(Duration(seconds: 2), (timer) {
+      if (_scrollController.hasClients) {
+        final maxScrollExtent = _scrollController.position.maxScrollExtent;
+        final currentScrollPosition = _scrollController.position.pixels;
+        final screenHeight = MediaQuery.of(context).size.height;
+
+        if (currentScrollPosition < maxScrollExtent) {
+          _scrollController.animateTo(
+            currentScrollPosition + screenHeight,
+            duration: Duration(seconds: 1),
+            curve: Curves.easeInOut,
+          );
+        } else {
+          _scrollController.animateTo(
+            0.0,
+            duration: Duration(seconds: 1),
+            curve: Curves.easeInOut,
+          );
+        }
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final cardWidth = 450.0; // Fixed width for the cards
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Page 2'),
+        title: Text('Offers'),
       ),
-      body: Center(
-        child: Text('This is Page 2'),
+      body: ListView.builder(
+        controller: _scrollController,
+        itemCount: offers.length,
+        itemBuilder: (context, index) {
+          return Container(
+            height: screenHeight,
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  elevation: 5,
+                  child: Container(
+                    width: cardWidth,
+                    child: ListTile(
+                      title: Center(child: Text(offers[index])),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: Page2(),
+  ));
 }
